51448bf31cd8819680f9265703b3fe9ddcde6288
Mike Schaeffer <mschaeffer@ksmpartners.com>
2013-10-02 11:17:23 -0400

    Add support for composite commands.

--- clj/src/rpn_calc/main.clj
+++ clj/src/rpn_calc/main.clj
@@ -35,7 +35,7 @@
   (doseq [[index val] (map list (range (count stack) 0 -1) (reverse stack) )]
     (printf "%d> %s\n" index val)))
 
-(defn apply-command [ command state ]
+(defn apply-command [ state command  ]
   (if-let [ state-update (command state)]
     (conj state state-update)
     false))
@@ -48,8 +48,13 @@
 (defn read-command [ str ]
   (find-command (read-string str)))
 
+(defn make-composite-command [ subcommands ]
+  (fn [ state ]
+    (reduce apply-command state subcommands)))
+
 (defn parse-command-string [ str ]
-  (read-command (.trim str)))
+  (make-composite-command
+   (map read-command (.split (.trim str) "\\s+"))))
 
 (defn -main []
   (loop [ state { :stack () :regs (vec (take 20 (repeat 0))) } ]
@@ -57,7 +62,7 @@
     (print "> ")
     (flush)
     (let [command (parse-command-string (.readLine *in*))]
-      (if-let [new-state (apply-command command state)]
+      (if-let [new-state (apply-command state command)]
         (recur new-state)
         nil))))
 
