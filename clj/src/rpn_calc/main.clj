86df96b57168d6100a408a08a3a3c1a7d7d355d8
Mike Schaeffer <mschaeffer@ksmpartners.com>
2013-10-02 09:25:09 -0400

    Switch the clojure implementation of rpncalc to use a hashmap for its environment rather than a clojure namespace/var.

--- clj/src/rpn_calc/main.clj
+++ clj/src/rpn_calc/main.clj
@@ -3,49 +3,48 @@
 (defn error [ message ]
   (println "Error: " message))
 
-(defn push [ x stack ]
-  (cons x stack))
+(def commands
+     {
+      '+ (fn [ stack ]
+             (let [[x y & more] stack]
+               (cons (+ y x) more)))
 
-(defn add [ stack ]
-  (let [[x y & more] stack]
-    (cons (+ y x) more)))
+      '- (fn [ stack ]
+             (let [[x y & more] stack]
+               (cons (- y x) more)))
 
-(defn sub [ stack ]
-  (let [[x y & more] stack]
-    (cons (- y x) more)))
+      '* (fn [ stack ]
+             (let [[x y & more] stack]
+               (cons (* y x) more)))
 
-(defn mul [ stack ]
-  (let [[x y & more] stack]
-    (cons (* y x) more)))
+      '/ (fn [ stack ]
+             (let [[x y & more] stack]
+               (cons (/ y x) more)))
 
-(defn div [ stack ]
-  (let [[x y & more] stack]
-    (cons (/ y x) more)))
+      'swap (fn [ stack ]
+              (let [[x y & more] stack]
+                (cons y (cons x more))))
 
-(defn swap [ stack ]
-  (let [[x y & more] stack]
-    (cons y (cons x more))))
+      'dup (fn [ stack ]
+             (let [[x & more] stack]
+               (cons x (cons x more))))
 
-(defn dup [ stack ]
-  (let [[x & more] stack]
-    (cons x (cons x more))))
+      'drop (fn [ stack ]
+              (let [[x & more] stack]
+                more))
 
-(defn drop1 [ stack ]
-  (let [[x & more] stack]
-    more))
-
-(defn quit [ stack ]
-  false)
+      'quit (fn [ stack ]
+              false)
+      })
 
 (defn show-stack [ stack ]
   (doseq [[index val] (map list (range (count stack) 0 -1) (reverse stack) )]
     (printf "%d> %s\n" index val)))
 
-(defn rpn-eval [ object stack ]
-  (binding [ *ns* (find-ns 'rpn-calc.main)]
-    (cond (number? object) (push object stack)
-          (symbol? object) ((eval object) stack)
-          :else (error "Bad value"))))
+(defn find-command [ object ]
+  (cond (number? object) (fn [ stack ] (cons object stack))
+        (symbol? object) (commands object)
+        :else (error "Bad value")))
 
 (defn read-command []
   (read-string (.trim (.readLine *in*))))
@@ -58,7 +57,7 @@
     (print "> ")
     (flush)
     (let [cmd (read-command)]
-      (if-let [new-stack (rpn-eval cmd stack)]
+      (if-let [new-stack ((find-command cmd) stack)]
         (recur new-stack)
         nil)))
   (println "end run."))
