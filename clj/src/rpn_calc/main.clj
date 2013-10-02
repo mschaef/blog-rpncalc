4c7ff89e6c14136da4391d7ad227dedaf28c16a7
Mike Schaeffer <mschaeffer@ksmpartners.com>
2013-10-02 10:27:09 -0400

    Switch to a composite representation of calculator state in the clojure version of the calculator.

--- clj/src/rpn_calc/main.clj
+++ clj/src/rpn_calc/main.clj
@@ -2,46 +2,44 @@
 
 (def commands
      {
-      '+ (fn [ stack ]
-             (let [[x y & more] stack]
-               (cons (+ y x) more)))
+      '+ (fn [ { [x y & more] :stack } ]
+           { :stack (cons (+ y x) more)})
+      
+      '- (fn [ { [x y & more] :stack } ]
+           { :stack (cons (- y x) more)})
 
-      '- (fn [ stack ]
-             (let [[x y & more] stack]
-               (cons (- y x) more)))
+      '* (fn [ { [x y & more] :stack } ]
+           { :stack (cons (* y x) more)})
 
-      '* (fn [ stack ]
-             (let [[x y & more] stack]
-               (cons (* y x) more)))
+      '/ (fn [ { [x y & more] :stack } ]
+           { :stack (cons (/ y x) more)})
 
-      '/ (fn [ stack ]
-             (let [[x y & more] stack]
-               (cons (/ y x) more)))
+      'swap (fn [ { [x y & more] :stack } ]
+              { :stack (cons y (cons x more))})
 
-      'swap (fn [ stack ]
-              (let [[x y & more] stack]
-                (cons y (cons x more))))
+      'dup (fn [ { [x & more] :stack } ]
+             { :stack (cons x (cons x more))})
 
-      'dup (fn [ stack ]
-             (let [[x & more] stack]
-               (cons x (cons x more))))
+      'drop (fn [ { [x & more] :stack } ]
+              { :stack more})
 
-      'drop (fn [ stack ]
-              (let [[x & more] stack]
-                more))
-
-      'quit (fn [ stack ]
+      'quit (fn [ {  } ]
               false)
       })
 
-(defn show-stack [ stack ]
+(defn show-state [ { stack :stack } ]
   (doseq [[index val] (map list (range (count stack) 0 -1) (reverse stack) )]
     (printf "%d> %s\n" index val)))
 
 (defn find-command [ object ]
   (if (number? object)
-    (fn [ stack ] (cons object stack))
+    (fn [ { stack :stack } ] { :stack (cons object stack) })
     (commands object)))
+
+(defn apply-command [ command state ]
+  (if-let [ state-update (command state)]
+    (conj state state-update)
+    false))
 
 (defn read-command []
   (read-string (.trim (.readLine *in*))))
@@ -49,13 +47,13 @@
 (defn -main
   "Main Entry point"
   []
-  (loop [ stack () ]
-    (show-stack stack)
+  (loop [ state { :stack () } ]
+    (show-state state)
     (print "> ")
     (flush)
-    (let [cmd (read-command)]
-      (if-let [new-stack ((find-command cmd) stack)]
-        (recur new-stack)
+    (let [command (find-command (read-command))]
+      (if-let [new-state (apply-command command state)]
+        (recur new-state)
         nil)))
   (println "end run."))
 
