eed316c0c9afc22f7359587b852337d22b5c17ba
Mike Schaeffer <mschaeffer@ksmpartners.com>
2013-11-25 10:52:14 -0500

    Bind nREPL sessions to port 53095.

--- clj/project.clj
+++ clj/project.clj
@@ -1,6 +1,14 @@
 (defproject rpn-calc "0.1.0-SNAPSHOT"
   :description "KSM Partners - RPN Calculator"
+
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.5.0"]]
+
+  :repl-options {
+                 :host "0.0.0.0"
+                 :port 53095
+                 }
+
   :main rpn-calc.main)
